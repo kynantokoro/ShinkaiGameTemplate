52a8747c31fdac2a8bba7cb80ef7b6cdf7fdf30f
kynantokoro <tokoro@kynantokoro.com>
2020-04-02 17:24:27 +0900

    some low res optimization

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t) 
     t.window.width = 1280                
     t.window.height = 1024               
-    t.window.borderless = true
-    t.window.display = 2
+    t.window.borderless = false
+    t.window.display = 1
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,22 @@
+push = require "push"
+GAMEWIDTH = 320
+GAMEHEIGHT = 256
+
 function love.load() 
-    
+    --graphics initialization
+    local windowWidth, windowHeight = love.graphics.getDimensions()
+    love.graphics.setDefaultFilter("nearest")
+    push:setupScreen(GAMEWIDTH, GAMEHEIGHT, windowWidth, windowHeight)
 end 
 
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
     end
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
 end
 
 function love.update(dt)
